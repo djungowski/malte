cc1e761606a8b3238df75f339fd7ee77179f78a1
Dominik Jungowski <Dominik@Jungowski.de>
2015-10-07 14:07:05 +0200

    Loading overlay

--- public/js/malte.js
+++ public/js/malte.js
@@ -1,16 +1,56 @@
 $(window).load(function() {
+  var socket;
+  var overlay = $('#overlay');
   var host = location.origin.replace(/^http/, 'ws');
-  var socket = new WebSocket(host);
-  $('.eat').click(function() {
-    var time = $(this).attr('data-time');
-    socket.send(time);
-  });
+  var connecting = false;
 
-  socket.onmessage = function(message) {
+  var hideOverlay = function() {
+    overlay.hide();
+  };
+
+  var showOverlay = function() {
+    overlay.show();
+  };
+
+  var connected = function() {
+    hideOverlay();
+    connecting = false;
+  }
+
+  var reconnect = function() {
+    if (connecting) {
+      return;
+    }
+    connecting = false;
+    showOverlay();
+    // Only try to reconnect every second
+    window.setTimeout(function() {
+      connect();
+    }, 1000);
+  };
+
+  var playMessage = function(message) {
     var time = message.data;
     var soundElement = $('#audio-eat-' + time)[0];
     soundElement.pause();
     soundElement.currentTime = 0;
     soundElement.play();
   };
+
+  var connect = function () {
+    connecting = true;
+    socket = new WebSocket(host);
+    socket.onopen = connected;
+    socket.onclose = reconnect;
+    socket.onerror = reconnect;
+    socket.onmessage = playMessage;
+  };
+
+
+  connect();
+  $('.eat').click(function() {
+    var time = $(this).attr('data-time');
+    socket.send(time);
+  });
+
 });
