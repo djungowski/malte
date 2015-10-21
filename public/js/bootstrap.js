1f8f0c617e1e5e495cac0306e8fe0f016d64e1de
Dominik Jungowski <Dominik@Jungowski.de>
2015-10-21 16:41:35 +0200

    replace malte.js with HungerController

--- public/js/bootstrap.js
+++ public/js/bootstrap.js
@@ -1,2 +1,3 @@
 var virtualMalteApp = angular.module('virtualMalte', []);
 virtualMalteApp.controller('NameController', VirtualMalte.NameController);
+virtualMalteApp.controller('HungerController', VirtualMalte.HungerController);
