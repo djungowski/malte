3cd034557be02f241efe476f8bcc79dfc47c0867
Dominik Jungowski <Dominik@Jungowski.de>
2015-10-22 12:25:38 +0200

    ignore files and run in prestart

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ gulp.task('dist', function () {
 	gulp.src('./node_modules/moment/moment.js')
 		.pipe(gulp.dest('./public/js'));
 
-	gulp.src('./node_modules/angular')
-		.pipe(gulp.dest('./public/js'));
+	gulp.src('./node_modules/angular/**/*')
+		.pipe(gulp.dest('./public/js/angular'));
 });
