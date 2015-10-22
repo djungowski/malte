f1c9068a71a78b9d77366ac1f4c4ec04370f28ef
Dominik Jungowski <Dominik@Jungowski.de>
2015-10-22 14:17:00 +0200

    even more stuff

--- spec/lunch-time-spec.js
+++ spec/lunch-time-spec.js
@@ -8,6 +8,7 @@ describe('lunch-time specs', function() {
 
 	afterEach(function() {
 	    jasmine.clock().uninstall();
+		this.lunchTime.reset();
 	});
 
     describe('#setNextLunchTime', function() {
@@ -34,4 +35,32 @@ describe('lunch-time specs', function() {
 			expect(lunchTime.format('YYYY-MM-DD HH:mm')).toEqual('2015-10-19 11:45');
 		});
     });
+
+	describe('#get', function() {
+	    it('is null by default', function() {
+	        expect(this.lunchTime.get()).toBeNull();
+	    });
+	});
+
+	describe('#setNextLunchTimeIfNull', function () {
+		it('sets the next lunch time', function () {
+			var mockedDate = new Date(2015, 9, 18, 9, 27, 67);
+			jasmine.clock().mockDate(mockedDate);
+			this.lunchTime.setNextLunchTimeIfNull();
+			var lunchTime = this.lunchTime.get();
+			expect(lunchTime.format('YYYY-MM-DD HH:mm')).toEqual('2015-10-18 11:45');
+		});
+
+		it('does not set next lunch time if already set', function () {
+			var mockedDate = new Date(2015, 9, 22, 9, 27, 67);
+			jasmine.clock().mockDate(mockedDate);
+
+			this.lunchTime.setNextLunchTimeIfNull();
+			mockedDate = new Date(2015, 9, 18, 9, 27, 67);
+			jasmine.clock().mockDate(mockedDate);
+			this.lunchTime.setNextLunchTimeIfNull();
+			var lunchTime = this.lunchTime.get();
+			expect(lunchTime.format('YYYY-MM-DD HH:mm')).toEqual('2015-10-22 11:45');
+		});
+	});
 });
